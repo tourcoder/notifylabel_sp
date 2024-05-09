b5b838adee4cce50eb13fad8a792a4d6fb2536d0
huabin <huabin@users.noreply.github.com>
2024-05-09 13:23:56 +0800

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# NotifyLabel on Swift Pack
+a simple pop-ups label
+
+On cocoapods, please check [here](https://github.com/tourcoder/notifylabel)
+
+### License
+
+MIT
---
+++ Tests/notifylabel-spTests/notifylabel_spTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import notifylabel_sp
+
+final class notifylabel_spTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documentation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
